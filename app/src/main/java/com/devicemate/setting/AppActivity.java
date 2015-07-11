cfbb2d12bfddd31807411583fdc23112c9e2a8c1
Kavindu Dodanduwa <Kavindudodanduwa@yahoo.com>
2015-07-11 14:57:52 +0530

    added setings

--- app/src/main/java/com/devicemate/setting/AppActivity.java
+++ app/src/main/java/com/devicemate/setting/AppActivity.java
@@ -1,12 +1,17 @@
 package com.devicemate.setting;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 
 import com.devicemate.R;
@@ -15,12 +20,39 @@ import com.devicemate.core.FdActivity;
 
 public class AppActivity extends Activity {
 
-	Button button;
+	private Button button;
+	private SeekBar seekBar ;
+	private EditText editText ;
+
+    private boolean saved;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.device_mate_setting);
+
+        saved = false;
+
+		seekBar = (SeekBar) findViewById(R.id.myseekBar);
+		editText = (EditText) findViewById(R.id.progressValue);
+
+		seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				editText.setText(Integer.toString(progress) + " S");
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+
+			}
+		});
+
 		addListenerOnButton();
 	}
 
@@ -28,20 +60,44 @@ public class AppActivity extends Activity {
 
 		final Context context = this;
 
-		button = (Button) findViewById(R.id.button1);
+		button = (Button) findViewById(R.id.nextbutton);
 
 		button.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View arg0) {
+            @Override
+            public void onClick(View arg0) {
 
-				Intent intent = new Intent(context, FdActivity.class);
-                startActivity(intent);   
+                if (saved) {
+                    Intent intent = new Intent(context, FdActivity.class);
+                    startActivity(intent);
+                } else {
+                    showDialog();
+                }
+            }
 
-			}
-
-		});
-
+        });
 	}
+
+    public void showDialog(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage("You need to set the time first.")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+        AlertDialog alert = alertDialog.create();
+        alert.show();
+    }
+
+    public void saveMessage(View view){
+
+        saved =true;
+        SharedPreferences sharedPreferences = getSharedPreferences("TimeDelay",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(getString(R.string.delayKey),seekBar.getProgress());
+        editor.commit();
+
+    }
 
 }
